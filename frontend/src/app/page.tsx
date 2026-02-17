70e898916cb7d1da3930fd37932e027455d77aa5
AkramBFS <mohammed_akram.boufas@univ-constantine2.dz>
2026-02-17 16:00:45 +0100

    feat: added content section and stats section

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,15 @@
 import HeroSection from "@/components/hero-section";
+import Features from "@/components/features";
+import ContentSection from "@/components/content";
+import Stats from "@/components/stats";
 
 export default function HomePage() {
   return (
     <div>
       <HeroSection />
+      <Features />
+      <ContentSection />
+      <Stats />
     </div>
   );
 }
