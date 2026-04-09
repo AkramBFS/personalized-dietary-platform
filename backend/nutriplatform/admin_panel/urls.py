2d258d578b963e01ad8f6af5f2a6fd7eddefad8f
soheib <bouafiasoheib918@gmail.com>
2026-04-09 11:56:38 +0100

    this is

--- backend/nutriplatform/admin_panel/urls.py
+++ backend/nutriplatform/admin_panel/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import CountryListView, GoalListView, SpecializationListView, LanguageListView 
+from .views import CountryListView, GoalListView, SpecializationListView
 
 from nutritionist.views_admin import (
     PendingNutritionistListView,
@@ -12,7 +12,6 @@ urlpatterns = [
     path('countries/',       CountryListView.as_view(),       name='countries'),
     path('goals/',           GoalListView.as_view(),          name='goals'),
     path('specializations/', SpecializationListView.as_view(),name='specializations'),
-    path('languages/',       LanguageListView.as_view(),      name='languages'),
     path('nutritionists/pending/',          PendingNutritionistListView.as_view()),
     path('nutritionists/<int:pk>/',         NutritionistDetailAdminView.as_view()),
     path('nutritionists/<int:pk>/approve/', ApproveNutritionistView.as_view()),
