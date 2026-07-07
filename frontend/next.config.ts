dcf9688a1de9559efbfcd12985156d32c8d124e3
Mohamed Akram Boufas <mohammed_akram.boufas@univ-constantine2.dz>
2026-07-07 19:34:06 +0100

    "feat: fixes"

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: 'standalone', // Trims node_modules and cache down to the absolute essentials
   images: {
     remotePatterns: [
       {
